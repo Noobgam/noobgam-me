2cf0bfd64a625e6cf02b1136b47e71a1c69d369f
Noobgam <shessmaster12@gmail.com>
2023-07-30 22:37:12 +0200

    feat(anki): iframe non-static

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
                         What am I up to?
                     </span>
                     <iframe
-                        src="https://grafana.noobgam.com/d-solo/ce95cd2d-acf6-4682-a8a1-df42e8a73a76/noobgam-personal-dashboard?orgId=1&from=1690701118837&to=1690722718837&theme=dark&panelId=1"
+                        src="https://grafana.noobgam.com/d-solo/ce95cd2d-acf6-4682-a8a1-df42e8a73a76/noobgam-personal-dashboard?orgId=1&panelId=1"
                         width="450" height="200" frameBorder="0"/>
                 </header>
             </div>
